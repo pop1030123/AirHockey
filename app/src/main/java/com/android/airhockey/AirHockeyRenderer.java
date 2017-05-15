fdb07ade3b04ef5cab99ce50e16ee21eb97f77d5
pengfu <77276575@qq.com>
2017-05-15 23:16:35 +0800

    varying the table color.

--- app/src/main/java/com/android/airhockey/AirHockeyRenderer.java
+++ app/src/main/java/com/android/airhockey/AirHockeyRenderer.java
@@ -18,6 +18,7 @@ import static android.opengl.GLES20.GL_FLOAT;
 import static android.opengl.GLES20.GL_LINES;
 import static android.opengl.GLES20.GL_POINTS;
 import static android.opengl.GLES20.GL_TRIANGLES;
+import static android.opengl.GLES20.GL_TRIANGLE_FAN;
 import static android.opengl.GLES20.glClear;
 import static android.opengl.GLES20.glClearColor;
 import static android.opengl.GLES20.glDrawArrays;
@@ -36,11 +37,13 @@ import static android.opengl.GLES20.glViewport;
 public class AirHockeyRenderer implements GLSurfaceView.Renderer {
 
     private static final int POSITION_COMPONENT_COUNT = 2 ;
+    private static final int COLOR_COMPONENT_COUNT = 3 ;
     private static final int BYTES_PER_FLOAT = 4 ;
+    private static final int STRIDE = (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) *BYTES_PER_FLOAT ;
     private final FloatBuffer vertexData ;
 
-    private static final String U_COLOR = "u_Color" ;
-    private int uColorLocation ;
+    private static final String A_COLOR = "a_Color" ;
+    private int aColorLocation ;
 
     private static final String A_POSITION = "a_Position" ;
     private int aPositionLocation ;
@@ -51,20 +54,19 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
     public AirHockeyRenderer(Context context){
         mContext = context ;
         float [] tableVertices = {
-                // first triangle
-                -0.5f ,-0.5f ,
-                0.5f ,0.5f ,
-                -0.5f ,0.5f ,
-                // second triangle
-                -0.5f, -0.5f ,
-                0.5f ,-0.5f ,
-                0.5f,0.5f ,
+                // triangle fan
+                   0f ,   0f ,1f ,1f ,1f ,
+                -0.5f ,-0.5f ,0.7f,0.7f,0.7f,
+                 0.5f ,-0.5f ,0.7f,0.7f,0.7f,
+                 0.5f , 0.5f ,0.7f,0.7f,0.7f,
+                -0.5f , 0.5f ,0.7f,0.7f,0.7f,
+                -0.5f, -0.5f ,0.7f,0.7f,0.7f,
                 // line
-                -0.5f ,0f ,
-                0.5f ,0f ,
+                -0.5f ,0f ,1f,0f,0f,
+                 0.5f ,0f ,1f,0f,0f,
                 // mallets
-                0f ,-0.25f ,
-                0f ,0.25f
+                0f ,-0.25f ,0f,0f,1f,
+                0f , 0.25f ,1f,0f,0f
         } ;
         vertexData = ByteBuffer.allocateDirect(tableVertices.length* BYTES_PER_FLOAT)
                 .order(ByteOrder.nativeOrder())
@@ -86,13 +88,18 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
         ShaderHelper.validateProgram(program) ;
         glUseProgram(program);
 
-        uColorLocation = glGetUniformLocation(program ,U_COLOR) ;
+        aColorLocation = glGetAttribLocation(program ,A_COLOR) ;
         aPositionLocation = glGetAttribLocation(program ,A_POSITION) ;
 
         vertexData.position(0) ;
-        glVertexAttribPointer(aPositionLocation ,POSITION_COMPONENT_COUNT ,GL_FLOAT ,false ,0 ,vertexData);
+        glVertexAttribPointer(aPositionLocation ,POSITION_COMPONENT_COUNT ,GL_FLOAT ,false ,STRIDE ,vertexData);
 
         glEnableVertexAttribArray(aPositionLocation);
+
+        vertexData.position(POSITION_COMPONENT_COUNT) ;
+        glVertexAttribPointer(aColorLocation ,COLOR_COMPONENT_COUNT ,GL_FLOAT ,false ,STRIDE ,vertexData);
+
+        glEnableVertexAttribArray(aColorLocation);
     }
 
     @Override
@@ -104,15 +111,11 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
     public void onDrawFrame(GL10 gl) {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUniform4f(uColorLocation ,1.0f ,1f ,1f ,1f);
-        glDrawArrays(GL_TRIANGLES ,0 ,6);
+        glDrawArrays(GL_TRIANGLE_FAN ,0 ,6);
 
-        glUniform4f(uColorLocation ,1f ,0f ,0f ,1f);
         glDrawArrays(GL_LINES ,6 ,2);
 
-        glUniform4f(uColorLocation ,0f ,0f ,1f,1f);
         glDrawArrays(GL_POINTS ,8,1);
-        glUniform4f(uColorLocation ,1f ,0f ,0f ,1f) ;
         glDrawArrays(GL_POINTS ,9 ,1);
     }
 }
